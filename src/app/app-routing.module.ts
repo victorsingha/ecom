64869beb5b0a4eeeaee3c440ab27a9375f3f8904
victor-singha <vicunite2@gmail.com>
2022-01-15 18:12:43 +0530

    bootstrap5 test with landing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from 'src/pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "landing", component: LandingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LandingComponent } from 'src/pages/landing/landing.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,LandingComponent
   ],
   imports: [
     BrowserModule,
