db1921f438b768f95df016f5d83ba5819145f2b0
victor-singha <vicunite2@gmail.com>
2022-01-18 20:39:55 +0530

    base setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from 'src/pages/landing/landing.component';
+import { PagenotfoundComponent } from 'src/pages/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-  { path: "landing", component: LandingComponent },
+  { path: "", component: LandingComponent, pathMatch: 'full' },
+  {
+    path: '**',component: PagenotfoundComponent,pathMatch: 'full'
+  }
 ];
 
 @NgModule({
