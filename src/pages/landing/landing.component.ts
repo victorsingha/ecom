3d4f01e7010a9ce5bfebe96175df98a28ce651fc
victor-singha <vicunite2@gmail.com>
2022-01-22 15:32:52 +0530

    bootstrap + our teaam

--- src/pages/landing/landing.component.ts
+++ src/pages/landing/landing.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  public teams: any = [];
+
+  constructor() {
+
+    this.teams = [
+      {
+        name: "Pankaj Chauhan",
+        position: "Product Manager",
+        imageurl:"assets/images/pankaj.jpg"
+      },
+      {
+        name: "Victor Singha",
+        position: "Software Engineer",
+        imageurl:"assets/images/victor.jpg"
+      },
+      {
+        name: "Abhisekh",
+        position: "Product Designer",
+        imageurl:"assets/images/pankaj.jpg"
+      },
+      // {
+      //   name: "Himanshu Nete",
+      //   position: "Software Engineer",
+      //   imageurl:"assets/images/himanshu.jpg"
+      // }
+    ]
+
+  }
 
   ngOnInit(): void {
   }
 
+
+  aboutus(){
+    let aboutus = document.getElementById("aboutus");
+    if(aboutus){
+      aboutus.scrollIntoView({behavior:"smooth"});
+    }
+  }
 }
